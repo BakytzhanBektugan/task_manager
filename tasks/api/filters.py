541ac5783706cbe99093e0b91efc8ecf97df2cf2
BakytzhanBektugan <bakytzhan.bektugan@gmail.com>
2020-10-12 22:49:33 +0600

    Refactoring: secret key, db_passwords were hidden, added range filter by completion date of tasks

--- tasks/api/filters.py
+++ tasks/api/filters.py
@@ -15,7 +15,9 @@ class TasksFilter(filters.FilterSet):
     Фильтрация Task по статусу и запланированной дате завершения
     """
     status = filters.ChoiceFilter(choices=STATUS_CHOICES)
-    completion_date = filters.DateFilter(lookup_expr='lte')
+    completion_date = filters.DateFilter(field_name='completion_date')
+    completion_date_from = filters.DateFilter(field_name='completion_date', lookup_expr='gte')
+    completion_date_to = filters.DateFilter(field_name='completion_date', lookup_expr='lte')
 
     class Meta:
         model = Task
